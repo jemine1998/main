158d27ee360ae989fcf92d7fa8d4bea15a94ea5b
jemine1998 <jemine1998@hotmail.com>
2019-04-14 20:39:21 +0800

    updating and creating test

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -1,15 +1,16 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.address.testutil.TypicalFlashcards.getTypicalFlashBook;
+import static seedu.address.testutil.TypicalFlashcards.getTypicalFlashBook;
+import static seedu.address.testutil.TypicalSubjects.getTypicalSubjectBook;
 
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
-//import seedu.address.model.FlashBook;
+import seedu.address.model.FlashBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-//import seedu.address.model.UserPrefs;
+import seedu.address.model.UserPrefs;
 
 public class ClearCommandTest {
 
@@ -24,13 +25,13 @@ public class ClearCommandTest {
         assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
-    /*@Test
+    @Test
     public void execute_nonEmptyFlashBook_success() {
-        Model model = new ModelManager(getTypicalFlashBook(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalFlashBook(), new UserPrefs());
+        Model model = new ModelManager(getTypicalSubjectBook(), getTypicalFlashBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalSubjectBook(), getTypicalFlashBook(), new UserPrefs());
         expectedModel.setFlashBook(new FlashBook());
         expectedModel.commitFlashBook();
 
         assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
-    }*/
+    }
 }
--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -1,22 +1,24 @@
 package seedu.address.logic.commands;
 
-//import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-//import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.address.logic.commands.CommandTestUtil.deleteFirstFlashcard;
-//import static seedu.address.testutil.TypicalFlashcards.getTypicalFlashBook;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.deleteFirstFlashcard;
+import static seedu.address.testutil.TypicalFlashcards.getTypicalFlashBook;
+import static seedu.address.testutil.TypicalSubjects.getTypicalSubjectBook;
 
-//import org.junit.Before;
-//import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-//import seedu.address.logic.CommandHistory;
-//import seedu.address.model.Model;
-//import seedu.address.model.ModelManager;
-//import seedu.address.model.UserPrefs;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 public class UndoCommandTest {
 
-    /*private final Model model = new ModelManager(getTypicalFlashBook(), new UserPrefs());
-    private final Model expectedModel = new ModelManager(getTypicalFlashBook(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalSubjectBook(), getTypicalFlashBook(), new UserPrefs());
+    private final Model expectedModel = new ModelManager(getTypicalSubjectBook(),
+            getTypicalFlashBook(), new UserPrefs());
     private final CommandHistory commandHistory = new CommandHistory();
 
     @Before
@@ -41,5 +43,5 @@ public class UndoCommandTest {
 
         // no undoable states in model
         assertCommandFailure(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
-    }*/
+    }
 }
